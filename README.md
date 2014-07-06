df311a9e0a848968b2c5b73b85782fcfb36f1cef
Chirag <chirag@RawDawg-PC.com>
2014-07-06 18:31:27 -0500

    added menu and status viewing option

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A simple python script which will update your facebook status
 
 To use this script simply type the following in the terminal/commandPrompt from the directory that contains the script:
 
-python status.py 'yourFacebookEmail' 'yourFacebookPass' 'yourStatus'
+python status.py 'yourFacebookEmail' 'yourFacebookPass' 
 
 Note: your email, password, and status must be enclosed in single/double quotes
--- status.py
+++ status.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 import sys
 import mechanize
+from BeautifulSoup import BeautifulSoup
 
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
@@ -12,7 +15,26 @@ browser.form['email'] = sys.argv[1]
 browser.form['pass'] = sys.argv[2]
 browser.submit()
 
-browser._factory.is_html = True
-browser.select_form(nr=1)
-browser.form['status'] = sys.argv[3]
-browser.submit()
+x = 0
+
+while x != 3:
+	input = raw_input("Please select an option:\n\tTo update your status enter 1\n\tTo view your friends' statuses enter 2\n\tTo quit this script enter 3\n")
+	x = int(input)
+	if x == 1:
+		status = raw_input('Please enter the status you would like to post.')
+		browser._factory.is_html = True
+		browser.select_form(nr=1)
+		browser.form['status'] = status
+		browser.submit()
+	elif x == 2:
+		page = browser.open('https://m.facebook.com')
+		soup = BeautifulSoup(page.read())
+		list = soup.findAll('div', {'class':'ca cb cu'})
+		for l in list:
+			a = l.find('div', {'class':'cx', 'data-sigil':'mfeed_pivots_message feed-story-highlight-candidate'})
+			if not a is None:
+				print l.find('a', text=True)
+				print a.find('span', text=True)
+				print '\n'
+	elif x != 3:
+		print 'invalid input'
